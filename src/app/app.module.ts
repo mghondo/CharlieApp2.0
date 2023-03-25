8ae1d4e6070781fa33c36076d0e05cc553bd0d70
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-03-25 19:50:01 -0400

    Material installed and Material Module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { PeopleDetailComponent } from './people-detail/people-detail.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PeopleListComponent,
+    TaskListComponent,
+    PeopleDetailComponent,
+    TaskDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
